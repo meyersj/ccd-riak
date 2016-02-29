d163eda18f28c3649a25e891167ccbfdc973da6e
Jeffrey Meyers <meyers2@pdx.edu>
2016-02-29 15:41:27 -0800

    install go for loopdata loader

--- bin/install-riak.sh
+++ bin/install-riak.sh
@@ -10,5 +10,16 @@ apt-get install -y \
     apt-transport-https default-jdk \
     riak python-dev python-pip libffi-dev libssl-dev 
 pip install riak
+
+# install go
+curl -O https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
+tar -xvf go1.5.3.linux-amd64.tar.gz
+mv go /usr/local
+
+echo >> ~/.profile
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+source ~/.profile
+go get github.com/basho/riak-go-client
+
 echo FINISHED
 reboot
