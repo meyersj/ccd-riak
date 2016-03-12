36de9208654c52b948c69f694835508098a9bcb7
Jeffrey Meyers <meyers2@pdx.edu>
2016-03-12 14:50:55 -0800

    mapreduce for speeds over 100

--- src/mapreduce.py
+++ src/mapreduce.py
@@ -5,40 +5,33 @@ from riak import RiakMapReduce
 
 def speed_over_100(client):
     """
-        Example Map Reduce Job with Riak
-        count the number of occurences for each highway id in all the detectors
+        Count number of loopdata records with speed over 100
     """
     mr = RiakMapReduce(client)
     mr.add_bucket('loopdata')
+    
     mr.map("""
       function(v) {
         var data = Riak.mapValuesJson(v)[0];
-        //var highwayid = data.highwayid;
-        ejsLog('/tmp/map_reduce.log', JSON.stringify(data))
-        return [{d:data}];
-        //return [{d:[highwayid, 1]}];
+        if (data.speed > 100) {
+            return [{d:1}];
+        }
+        return [];
       }
     """)
+    
     mr.reduce("""
       function(values) {
-        var counts = {};
+        var count = 0;
         var i = 0;
         if (values[0].c) {
-          counts = values[0].c;
+          count = values[0].c;
           i = 1;
         }
-        /*
         for (; i < values.length; i++) {
-          var data = values[i].d;
-          if (data[0] in counts) {
-            counts[data[0]] += data[1]
-          }
-          else {
-            counts[data[0]] = data[1]
-          }
+          count += values[i].d;
         }
-        */
-        return [{c:counts}];
+        return [{c:count}];
       }
     """)
     response = mr.run()
@@ -86,4 +79,4 @@ def highway_detector_count(client):
 if __name__ == "__main__":
     client = connect()
     speed_over_100(client)
-    highway_detector_count(client)
+    #highway_detector_count(client)
